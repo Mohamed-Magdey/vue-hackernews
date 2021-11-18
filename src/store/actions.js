28b93850f7d23bab9017bd3485e3e5223d0b8f8c
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-11-18 15:03:47 +0200

    Refactor actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,8 @@
-export default {
-  fetchListData() {
+import { fetchListData } from '../api/api'
 
+export default {
+  fetchListData({ commit }, { type }) {
+    return fetchListData(type)
+      .then(items => commit('SET_ITEMS', { items }))
   }
 }
