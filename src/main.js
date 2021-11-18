10fc69c9416429e976c36874e13dc9f9cd849bc9
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-11-18 13:28:11 +0200

    Add Vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import ProgressBar from './components/ProgressBar'
+import storeConfig from './store/store-config'
 
+Vue.use(Vuex)
+
+const store = new Vuex.Store(storeConfig)
 Vue.config.productionTip = false
 
 const bar = new Vue(ProgressBar).$mount()
@@ -10,5 +15,6 @@ document.body.appendChild(bar.$el)
 
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 })
