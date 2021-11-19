0acab0e8b0516175c13b1fff14f4fa0b75c48bfb
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-11-19 15:26:39 +0200

    Setup vue router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App'
+import Router from 'vue-router'
+import routerConfig from './router/router-config'
 import ProgressBar from './components/ProgressBar'
 import storeConfig from './store/store-config'
 
 Vue.use(Vuex)
+Vue.use(Router)
 
+const router = new Router(routerConfig)
 const store = new Vuex.Store(storeConfig)
 Vue.config.productionTip = false
 
@@ -16,5 +20,6 @@ document.body.appendChild(bar.$el)
 new Vue({
   el: '#app',
   store,
+  router,
   render: h => h(App)
 })
