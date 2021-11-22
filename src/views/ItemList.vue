7644032bd7aa3477809d47663dcb29c511143a20
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-11-22 13:34:43 +0200

    Refactor ItemList component

--- src/views/ItemList.vue
+++ src/views/ItemList.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="item-list-view">
-    <span>
-      {{ $route.params.page || 1 }}/{{ $store.getters.maxPage }}
-    </span>
+    <div class="item-list-nav">
+      <router-link
+        v-if="page > 1"
+        :to="'/' + type + '/' + (page - 1)"
+      >
+        &lt; prev
+      </router-link>
+      <a v-else>&lt; prev</a>
+      <span>{{ page || 1 }}/{{ maxPage }}</span>
+      <router-link
+        v-if="(page || 1) < maxPage"
+        :to="'/' +  type + '/' + ((Number(page) || 1) + 1)"
+      >
+          more &gt;
+      </router-link>
+      <a v-else>more &gt;</a>
+    </div>
     <div class="item-list">
       <Item
         v-for="item in $store.getters.displayItems"
@@ -68,6 +82,23 @@ export default {
   margin: 30px 0;
   width: 100%;
   transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+}
+.item-list-nav {
+  padding: 15px 30px;
+  position: fixed;
+  text-align: center;
+  top: 55px;
+  left: 0;
+  right: 0;
+  z-index: 998;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+}
+.item-list-nav a {
+  margin: 0 1em;
+}
+.item-list-nav .disabled {
+  color: #ccc;
 }
 @media (max-width: 600px) {
   .item-list {
