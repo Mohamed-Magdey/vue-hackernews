1053ff4565691ba8a57d352db04bada49da1aebb
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-11-22 19:59:40 +0200

    Add timeAgo filter

--- src/util/filters.js
+++ src/util/filters.js
@@ -9,3 +9,22 @@ export function host(url) {
   }
   return parts.join('.')
 }
+
+export function timeAgo(time) {
+  const between = Date.now() / 1000 - Number(time) // #A
+  if (between < 3600) { // #B
+    return pluralize((between / 60), ' minute') // #C
+  } else if (between < 86400) {
+    return pluralize((between / 3600), ' hour')
+  } else {
+    return pluralize((between / 86400), ' day')
+  }
+}
+
+function pluralize(time, label) { // #D
+  const roundedTime = Math.round(time) // #E
+  if (roundedTime === 1) {
+    return roundedTime + label
+  }
+  return roundedTime + label + 's'
+}
